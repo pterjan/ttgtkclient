04eaa347dfb0b3b05f99c9df9e2742170d54bf25
Pascal Terjan <pterjan@gmail.com>
2012-01-10 23:27:26 +0000

    Handle enter in the password field

--- lib/ttgtkclient/ttgtkclient.rb
+++ lib/ttgtkclient/ttgtkclient.rb
@@ -43,6 +43,7 @@ class TTGtkClient
 		pass_entry = Gtk::Entry.new
 		pass_entry.visibility = false
 		pass_label.mnemonic_widget = pass_entry
+		pass_entry.signal_connect('activate'){ dialog.response(Gtk::Dialog::RESPONSE_ACCEPT) }
 		table.attach(pass_label, 0, 1, 2, 3, Gtk::FILL, Gtk::FILL, 3, 3)
 		table.attach(pass_entry, 1, 2, 2, 3, Gtk::EXPAND, Gtk::FILL, 3, 3)
 		dialog.vbox.add(table)
